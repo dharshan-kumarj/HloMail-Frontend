2ce5ffda7a1628a0271aa4bdb5973621388a22a2
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-06-02 14:39:43 +0530

    updated login design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleEmailChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    const data = { email, password };
+
+    try {
+      const response = await fetch('http://localhost:8000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        // Save the token in the cookies
+        Cookies.set('token', responseData.token);
+        // Redirect to the dashboard
+        window.location.href = 'http://localhost:5173/dashboard';
+      } else {
+        setErrorMessage(responseData.message || 'An error occurred during login');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setErrorMessage('An error occurred. Please try again later.');
+    }
+  };
+
   return (
-<div className="container mx-auto">
-    <h1 className='container bg-black text-white'>Login in container</h1>
-</div>
+    <main className="vh-100 d-flex flex-column" style={{ backgroundColor: "#FFFFFF" }}>
+      <div className="container-fluid p-5" >
+        <div className="row justify-content-between align-items-center">
+            <div className="col-auto">
+              <div className="d-flex align-items-center mb-3">
+                <div className="col-12">
+                  <h1 className="text-start" style={{ color: "#aa14f0" }}>
+                    HloMail
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="col-auto d-flex align-items-center">
+              <button className="btn btn-outline-light me-3" style={{ backgroundColor: "#aa14f0",border: '1px solid #aa14f0' }}>
+                Back to Home
+              </button>
+              <button className="btn btn-outline-light" style={{ color: "#aa14f0", border: '1px solid #aa14f0' }} onClick={() => window.location.href = "http://localhost:5173/register"}>
+                Register
+              </button>
+            </div>
+        </div>
+      </div>
+      <div className="container d-flex justify-content-center align-items-center flex-grow-1">
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-lg-8 col-xl-12">
+            <div className="card mx-auto" style={{ maxWidth: '800px', width: '600px', height: '600px', backgroundColor: "#FFFFFF", borderRight: '7px solid #aa14f0', borderBottom: '7px solid #aa14f0', marginTop: '-150px' }}>
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 className="card-title text-center fs-1 mb-4" style={{ color: "#aa14f0" }}>Login</h5>
+                <h3 className='fs-6 text-black' style={{ color: "#D9D9D9" }}> Sign in to your Account</h3>
+                {errorMessage && <div className="alert pt-2" style={{ backgroundColor: "#aa14f0" }} role="alert">{errorMessage}</div>}
+                <form onSubmit={handleSubmit} className="w-100">
+                  <div className="form-group fs-5 p-3">
+                    <input
+                      type="email"
+                      className="form-control fs-6 form-control-lg"
+                      id="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      placeholder="Enter email"
+                      required
+                    />
+                  </div>
+                  <div className="form-group fs-5 p-3">
+                    <input
+                      type="password"
+                      className="form-control fs-6 form-control-lg"
+                      id="password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  <div className="d-flex pt-3 d-grid gap-2 col-12 mx-auto justify-content-center">
+                    <button type="submit" className="btn btn-lg text-white form-control" style={{ backgroundColor: "#aa14f0" }}>
+                      Login
+                    </button>
+                  </div>
+                  <h3 className='fs-6 pt-4 d-flex justify-content-center text-black' style={{ color: "#D9D9D9" }}>I forgot my password.<span style={{ color: "#aa14f0", marginLeft: "10px" }} onClick={() => window.location.href = "http://localhost:5173/forgotpass"}> Click here.</span></h3>
+                  <div className="d-grid pt-3 mb-5 gap-2 col-12 mx-auto">
+                    <button className="btn btn-outline-light text-black form-control" type="button" style={{border: '1px solid #aa14f0'}} onClick={() => window.location.href = "http://localhost:5173/register"}>Register New Account</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
-};
+}
 
 export default Login;
