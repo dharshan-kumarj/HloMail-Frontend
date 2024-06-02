2521f39e7ddc7dc34a11a475f9de61fe52db15ad
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-06-02 13:56:34 +0530

    updated bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap/dist/css/bootstrap-utilities.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
   return (
 <div className="container mx-auto">
-    <h1>Login in container</h1>
+    <h1 className='container bg-black text-white'>Login in container</h1>
 </div>
   );
 };
