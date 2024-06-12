d3c975ed2cbec9adef95e5174a1e94bc54e367b1
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-06-12 13:40:58 +0530

    Update in Dashboard Home

--- src/components/DashboardHome.tsx
+++ src/components/DashboardHome.tsx
@@ -1,53 +1,70 @@
 // src/components/Dashboard.tsx
-import  { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/dashboard.css';
-import Cookies from 'js-cookie';
-import ChartComponent from './ChartComponent';
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/dashboard.css";
+import Cookies from "js-cookie";
+import ChartComponent from "./ChartComponent";
 
-interface Props{
-  api_key :string;
+interface Props {
+  api_key: string;
 }
-const DashboardHome = ({api_key}:Props) => {
+
+function find_today_intraction(interactions: { [x: number]: number; }){
+  var interaction = 0
+  for (var key in interactions){
+    interaction += interactions[key]
+  }
+  return interaction
+}
+const DashboardHome = ({ api_key }: Props) => {
   const [credit, setCredit] = useState(false);
-  const [interactionData, setInteractionData] = useState<Record<string, number>>({});
-  const [timePeriod, setTimePeriod] = useState('day');
-
+  const [interactionData, setInteractionData] = useState<
+    Record<string, number>
+  >({});
+  const [timePeriod, setTimePeriod] = useState("day");
+  const [todayIntraction, setTodayIntraction] = useState(0);
+ 
   const FetchData = async () => {
-    const token = Cookies.get('token');
-    
-
+    const token = Cookies.get("token");
 
     try {
-      const response = await fetch('https://hlomail.sanjaysagar.com/api-dashboard', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify({ api_key: api_key })
-      });
+      const response = await fetch(
+        "https://hlomail.sanjaysagar.com/api-dashboard",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          
+          body: JSON.stringify({ api_key: api_key=="*"?null:api_key }),
+        }
+      );
 
       if (response.status === 401) {
-        window.location.href = 'https://hlomail-frontend.sanjaysagar.com/login';
+        window.location.href = "https://hlomail-frontend.sanjaysagar.com/login";
         return;
       }
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const data = await response.json();
       setCredit(data.credit);
+      setTodayIntraction( find_today_intraction(data.today_intraction));
+      console.log(todayIntraction)
       const filledData = ensureMinimumDataPoints(data.today_intraction);
       setInteractionData(filledData);
-      console.log('API Response:', data);
+      console.log("API Response:", data);
     } catch (error) {
-      console.error('API Error:', error);
+      console.error("API Error:", error);
     }
   };
 
-  const ensureMinimumDataPoints = (data: Record<string, number>): Record<string, number> => {
+  const ensureMinimumDataPoints = (
+    data: Record<string, number>
+  ): Record<string, number> => {
     const labels = Object.keys(data);
     const values = Object.values(data);
 
@@ -72,35 +89,34 @@ const DashboardHome = ({api_key}:Props) => {
   };
 
   const fetchInteractionData = async (timePeriod: string) => {
-    const token = Cookies.get('token');
-   
+    const token = Cookies.get("token");
 
     try {
-      const response = await fetch('https://hlomail.sanjaysagar.com/logs', {
-        method: 'POST',
+      const response = await fetch("https://hlomail.sanjaysagar.com/logs", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ api_key: api_key, time_period: timePeriod })
+        body: JSON.stringify({ api_key: api_key=="*"?null:api_key , time_period: timePeriod }),
       });
 
       if (response.status === 401) {
-        window.location.href = 'https://hlomail-frontend.sanjaysagar.com/login';
+        window.location.href = "https://hlomail-frontend.sanjaysagar.com/login";
         return;
       }
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const data = await response.json();
       console.log(data);
       const filledData = ensureMinimumDataPoints(data.interactions);
       setInteractionData(filledData);
-      console.log('API Logs Response:', data);
+      console.log("API Logs Response:", data);
     } catch (error) {
-      console.log('API Logs Error:', error);
+      console.log("API Logs Error:", error);
     }
   };
 
@@ -114,14 +130,14 @@ const DashboardHome = ({api_key}:Props) => {
 
   return (
     <>
-      
       <div className="main-content">
         <div className="container-fluid">
           <div className="d-flex justify-content-between align-items-center mb-3">
             <div>
               <h1>Dashboard</h1>
               <p>
-                Welcome to your API's dashboard, Effortless API Integration for Smarter Communication.
+                Welcome to your API's dashboard, Effortless API Integration for
+                Smarter Communication.
               </p>
             </div>
           </div>
@@ -138,7 +154,7 @@ const DashboardHome = ({api_key}:Props) => {
               <div className="card shadow bg-white rounded">
                 <div className="card-body">
                   <h5 className="card-title">Interactions</h5>
-                  <p className="card-text">11 Interactions today</p>
+                  <p className="card-text">{todayIntraction} Interactions today</p>
                 </div>
               </div>
             </div>
@@ -155,16 +171,28 @@ const DashboardHome = ({api_key}:Props) => {
                 <div className="col">
                   <h5>Interactions</h5>
                 </div>
-                <div className="col-2 d-none d-lg-flex" onClick={() => setTimePeriod('day')}>
+                <div
+                  className="col-2 d-none d-lg-flex"
+                  onClick={() => setTimePeriod("day")}
+                >
                   <p>Day</p>
                 </div>
-                <div className="col-2 d-none d-lg-flex" onClick={() => setTimePeriod('week')}>
+                <div
+                  className="col-2 d-none d-lg-flex"
+                  onClick={() => setTimePeriod("week")}
+                >
                   <p>Week</p>
                 </div>
-                <div className="col-2 d-none d-lg-flex" onClick={() => setTimePeriod('month')}>
+                <div
+                  className="col-2 d-none d-lg-flex"
+                  onClick={() => setTimePeriod("month")}
+                >
                   <p>Month</p>
                 </div>
-                <div className="col-2 d-none d-lg-flex" onClick={() => setTimePeriod('year')}>
+                <div
+                  className="col-2 d-none d-lg-flex"
+                  onClick={() => setTimePeriod("year")}
+                >
                   <p>Year</p>
                 </div>
               </div>
